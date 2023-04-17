ca861aff7b6d979c3bb6597217ca01c1dc3b49bd
Mack <francoislml@Franck.lan>
2023-04-17 13:13:00 +0200

    first commit

---
+++ testTsinor.js
@@ -0,0 +1,15 @@
+const numTarget = (nums, target) => {
+  let stock = [];
+
+  for (i = 0; i < nums.length; i++) {
+    const good = target - nums[i];
+
+    if (good in stock) {
+      return [stock[good], i];
+    }
+
+    stock[nums[i]] = i;
+  }
+};
+
+console.log(numTarget([3, 4, 6, 2, 12], 9));
